8a60ae278f4617dc3613592158d2b1cc73f2710a
Alex E.T. Snyder <alexetsnyder@gmail.com>
2024-08-04 14:11:14 -0400

    SFML Hello World
    Statically linked to the SFML library, and created a basic example with a window, and a circle drawn to it.

--- SeaEngine/Main.cpp
+++ SeaEngine/Main.cpp
@@ -1,10 +1,29 @@
+#include <SFML/Graphics.hpp>
+
 #include <cstdlib>
 #include <iostream>
 
-
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World!\n";
+	sf::RenderWindow window{ sf::VideoMode(200, 200), "SeaEngine" };
+	sf::CircleShape circle{ 100.0f };
+	circle.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+
+			window.clear();
+			window.draw(circle);
+			window.display();
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
